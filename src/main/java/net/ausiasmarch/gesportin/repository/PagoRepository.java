39e355a6481c62ae39d0ac928ac9829d627c7df3
Linztark <anasilvesttttre@gmail.com>
2026-01-31 21:00:13 +0100

    repository basado en jugador

--- src/main/java/net/ausiasmarch/gesportin/repository/PagoRepository.java
+++ src/main/java/net/ausiasmarch/gesportin/repository/PagoRepository.java
@@ -9,13 +9,19 @@ import net.ausiasmarch.gesportin.entity.PagoEntity;
 
 public interface PagoRepository extends JpaRepository<PagoEntity, Long> {
     
-    Page<PagoEntity> findByAbonadoTrue(Pageable oPageable);
-
-    Page<PagoEntity> findByAbonadoFalse(Pageable oPageable);
-
-    PagoEntity findByIdAndAbonadoTrue(Long id);
+    Page<PagoEntity> findByCuotaDescripcionContainingIgnoreCase (String descripcion, Pageable oPageable);
 
     Page<PagoEntity> findByCuotaId(Long idCuota, Pageable oPageable);
 
     Page<PagoEntity> findByJugadorId(Long idJugador, Pageable oPageable);
+
+    Page<PagoEntity> findByJugadorUsuarioId(Long idUsuario, Pageable oPageable);
+
+    Page<PagoEntity> findByJugadorEquipoId(Long idEquipo, Pageable oPageable);
+
+    Page<PagoEntity> findByCuotaEquipoId(Long idEquipo, Pageable oPageable);
+
+    Page<PagoEntity> findByAbonado(Integer abonado, Pageable oPageable);
+
+    Page<PagoEntity> findByIdAndAbonado(Long id, Integer abonado);
 }
